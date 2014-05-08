3afbff133bf348e56116cc03635714cfb6437572
Gregory Kramida <algomorph@gmail.com>
2014-05-08 09:08:28 -0400

    Added numpy core include
    /usr/local/lib/dist-packages/numpy/core/include

--- Debug/src/subdir.mk
+++ Debug/src/subdir.mk
@@ -6,16 +6,19 @@
 CPP_SRCS += \
 ../src/Bitcounting.cpp \
 ../src/CSExtraction.cpp \
+../src/OpenCVFilters.cpp \
 ../src/module.cpp 
 
 OBJS += \
 ./src/Bitcounting.o \
 ./src/CSExtraction.o \
+./src/OpenCVFilters.o \
 ./src/module.o 
 
 CPP_DEPS += \
 ./src/Bitcounting.d \
 ./src/CSExtraction.d \
+./src/OpenCVFilters.d \
 ./src/module.d 
 
 
@@ -23,7 +26,7 @@ CPP_DEPS += \
 src/%.o: ../src/%.cpp
 	@echo 'Building file: $<'
 	@echo 'Invoking: GCC C++ Compiler'
-	g++ -std=c++0x -I"/home/algomorph/Factory/gpxanalyzer/include" -I/usr/local/include/python2.7 -I/usr/include/python2.7 -O0 -g3 -Wall -c -fmessage-length=0  -fopenmp  -lgomp -fPIC -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.d)" -o "$@" "$<"
+	g++ -std=c++0x -I"/home/algomorph/Factory/gpxanalyzer/include" -I/usr/local/include/python2.7 -I/usr/local/lib/python2.7/dist-packages/numpy/core/include -I/usr/include/python2.7 -O0 -g3 -Wall -c -fmessage-length=0  -fopenmp  -lgomp -fPIC -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.d)" -o "$@" "$<"
 	@echo 'Finished building: $<'
 	@echo ' '
 
