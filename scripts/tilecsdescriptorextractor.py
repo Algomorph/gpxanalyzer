8c2d4bc9ebd5be1ab21615b9126ed2812c791ebb
Gregory Kramida <algomorph@gmail.com>
2014-05-08 09:06:52 -0400

    Argparse bugs fixed in tile descriptor extractor script.

--- scripts/tilecsdescriptorextractor.py
+++ scripts/tilecsdescriptorextractor.py
@@ -18,6 +18,7 @@ import pyopencl as cl
 import sys
 import os
 import math
+import gpxanalyzer.utils.console as console
 from PIL import Image
 
 
@@ -26,19 +27,21 @@ parser = argparse.ArgumentParser(description="A tool for extracting MPEG-7 Color
 parser.add_argument('-w', '--channel_clusters', nargs='+', type=int,
                     help="Distance to which of the cluster means"+
                     " to represent as R,G,B colors in a mean distance image",default=[1,2,3])
-parser.add_argument("--cluster", "-c", type=bool,action='store_true',
+parser.add_argument("--cluster", "-c",action='store_true',
                     help="generate and save cluster means",default=False)
-parser.add_argument("--mean_distance_image", "-m", type=bool,action='store_true',
+parser.add_argument("--mean_distance_image", "-m",action='store_true',
                     help="generate mean-distance image",default=False)
-parser.add_argument("--segmented_image", "-s", type=bool,action='store_true',
+parser.add_argument("--segmented_image", "-s", action='store_true',
                     help="generate segmented image",default=False)
 parser.add_argument("--number_of_clusters", "-k", type=int,default=3,
-                    help="number of clusters to process",default=False)
+                    help="number of clusters to generate")
 
 parser.add_argument("--input_path", "-i", default=None,
                     help="path to the image whose descriptor to compute")
 parser.add_argument("--output_path", "-o", default=None,
                     help="path to folder where to store the descriptors and output images")
+parser.add_argument("--verbose", "-v", action='store_true',
+                    help="generate mean-distance image",default=False)
 
 def find_dists(means,rowwise):
     dists = np.zeros((len(rowwise),len(means)),dtype=np.uint16)
@@ -48,6 +51,13 @@ def find_dists(means,rowwise):
 
 def save_dists_image(dists,path,name,which):
     n_means = dists.shape[1]
+    save_path = path+os.path.sep + name + \
+    "_{0:0d}_means_distances-{1:0d}-{2:0d}-{3:0d}.png".format(n_means,which[0],which[1],which[2])
+    
+    if(os.path.isfile(save_path)):
+        if not console.query_yes_no("File at {0:s} already exists. Overwrite?".format(save_path), "no"):
+            return
+    
     if(len(which)!= 3 or len(np.intersect1d(which, np.arange(n_means), True)) < len(which)):
         raise ValueError("which should contain indexes of means distances to which to represent as"+
                          " each of the three channels. It should be a length 3 array of indexes.")
@@ -56,25 +66,36 @@ def save_dists_image(dists,path,name,which):
     dists_norm = dists.astype(np.float32) / maxes
     out_size = int(math.sqrt(dists.shape[0]))
     raster_dists = dists_norm.reshape((out_size,out_size,n_means))[:,:,which]
-    Image.fromarray(raster_dists).save(path+os.path.sep + name + 
-                                       "_{0:0d}_means_distances-{1:0d}-{2:0d}-{3:0d}.png"\
-                                       .format(n_means,which[0],which[1],which[2]),"PNG")
+    Image.fromarray(raster_dists).save(save_path,"PNG")
 
-palette = np.array([[230,174,44],[245,241,22],[168,219,105],[135,208,212],[139,132,184],[173,34,12],[105,72,12],[49,125,2],[2,125,123],[117,98,122],[227,16,44]],dtype=np.uint8)
+palette = np.array([[230,174,44],
+                    [245,241,22],
+                    [168,219,105],
+                    [135,208,212],
+                    [139,132,184],
+                    [173,34,12],
+                    [105,72,12],
+                    [49,125,2],
+                    [2,125,123],
+                    [117,98,122],
+                    [227,16,44]],dtype=np.uint8)
 
     
 def save_segment_image(dists, path, name):
-    out_size = int(math.sqrt(dists.shape[0]))
     n_means = dists.shape[1]
+    save_path = path+os.path.sep + name + "_{0:0d}_segments.png".format(n_means)
+    if(os.path.isfile(save_path)):
+        if not console.query_yes_no("File at {0:s} already exists. Overwrite?".format(save_path), "no"):
+            return
+    out_size = int(math.sqrt(dists.shape[0]))
+    
     classes = np.argmin(dists,axis=1).reshape(out_size,out_size)
     segmented = np.zeros((out_size,out_size,3),dtype=np.uint8)
 
     for y in xrange(0,classes.shape[0]):
         for x in xrange(0,classes.shape[1]):
             segmented[y,x] = palette[classes[y,x]]
-    Image.fromarray(segmented).save(path+os.path.sep + name + 
-                                       "_{0:0d}_segments.png"\
-                                       .format(n_means),"PNG")
+    Image.fromarray(segmented).save(save_path,"PNG")
 
 def extract_descriptors(path, n_channels):
     device = system.get_devices_of_type(cl.device_type.GPU)[0]
@@ -109,10 +130,14 @@ if __name__ == '__main__':
                          " got {0:0d} x {1:0d}.".format(width,height))
     out_descriptor_path = args.output_path + os.path.sep + name + "_descriptors.npz"
     if(os.path.isfile(out_descriptor_path)):
+        if(args.verbose):
+            print "Found descriptor file at {0:s}, loading.".format(out_descriptor_path)
         descr_file = np.load(out_descriptor_path)
         descr = descr_file["descriptors"]
         descr_file.close()
     else:
+        if(args.verbose):
+            print "Generating descriptor file at {0:s}.".format(out_descriptor_path)
         descr = extract_descriptors(args.input_path, n_channels)
         np.savez_compressed(out_descriptor_path,descriptors = descr)
     k = args.number_of_clusters
@@ -120,12 +145,18 @@ if __name__ == '__main__':
     which = args.channel_clusters
     
     if(args.cluster or args.mean_distance_image or args.segmented_image):
+        if(args.verbose):
+            print "Computing & saving cluster centroids / means."
         means = cluster(args.output_path,name,k,rowwise)    
     
     if(args.mean_distance_image or args.segmented_image):
+        if(args.verbose):
+            print "Computing distance of every descriptor to each of the cluster means."
         dists = find_dists(means, rowwise)
     
     if(args.mean_distance_image):
+        if(args.verbose):
+            print "Computing distance of every descriptor to each of the cluster means."
         save_dists_image(dists, args.output_path, name, which)
     
     if(args.segmented_image):
